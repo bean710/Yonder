534d1ec41e95f2920482e012956e2b303df961ab
Ben Keener <keener4christ@gmail.com>
2021-04-29 21:30:51 -0400

    Added DB to Unity deserialization classes

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -18,12 +18,34 @@ public class ServerManager : MonoBehaviour
         {
             Debug.Log("Connected!!!");
 
-            InvokeRepeating("SendWebSocketPing", 0.0f, 60f * pingFrequency);
+            InvokeRepeating("SendWebSocketPing", 60f, 60f * pingFrequency);
+            GetForeignData();
         };
 
         webSocket.OnMessage += (bytes) =>
         {
             Debug.Log($"Message: {System.Text.Encoding.UTF8.GetString(bytes)}");
+        };
+
+        webSocket.OnMessage += (bytes) =>
+        {
+            string bytesData = System.Text.Encoding.UTF8.GetString(bytes);
+
+            WebSocketMessage message = JsonUtility.FromJson<WebSocketMessage>(bytesData);
+
+            if (message.type == "foreignWindowData")
+            {
+                CompleteDataResult foreignWindowData = JsonUtility.FromJson<CompleteDataResult>(bytesData);
+
+                foreach (CompleteDataResult.ForeignWindowData foreignWindow in foreignWindowData.data)
+                {
+                    Debug.Log($"Owner ID: {foreignWindow.ownerId}");
+                    Debug.Log($"Apartment ID: {foreignWindow.apartmentId}");
+                    Debug.Log($"Data: {foreignWindow.data}");
+
+                    Debug.Log($"Color: {foreignWindow.data.stickyNotes[0].color}");
+                }
+            }
         };
 
         await webSocket.Connect();
@@ -35,6 +57,15 @@ public class ServerManager : MonoBehaviour
     #if !UNITY_WEBGL || UNITY_EDITOR
         webSocket.DispatchMessageQueue();
     #endif 
+    }
+
+    async void GetForeignData()
+    {
+        if (webSocket.State == WebSocketState.Open)
+        {
+            Debug.Log("Getting foreign window data");
+            await webSocket.SendText("{'action' : 'getData'}");
+        }
     }
 
     async void SendWebSocketPing()
@@ -50,4 +81,43 @@ public class ServerManager : MonoBehaviour
     {
         await webSocket.Close();
     }
+}
+
+[System.Serializable]
+public class WebSocketMessage
+{
+    public string type;
+}
+
+[System.Serializable]
+public class CompleteDataResult
+{
+    [System.Serializable]
+    public class ForeignWindowData
+    {
+        [System.Serializable]
+        public class ForeignWindowObjectData
+        {
+            [System.Serializable]
+            public class StickyNoteData
+            {
+                [System.Serializable]
+                public class StickyNotePos
+                {
+                    public int x, y, z;
+                }
+
+                public StickyNotePos position;
+                public string color;
+            }
+
+            public List<StickyNoteData> stickyNotes;
+        }
+
+        public string apartmentId;
+        public string ownerId;
+        public ForeignWindowObjectData data;
+    }
+
+    public List<ForeignWindowData> data;
 }
